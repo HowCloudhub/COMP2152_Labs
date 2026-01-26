5ff39ae6d3d56411e021d005e64ca6bee23bb438
HowCloudhub <howlader101@gmail.com>
2026-01-26 06:43:44 -0500

    update lab01

--- Week01/Lab01.py
+++ Week01/Lab01.py
@@ -2,5 +2,25 @@
 #Sample Code
 
 
-#Question 2: Defining an array(List)
+# Question 2: Defining an array(List)
 my_array =[1,4,7,9]
+
+# Questiond 3: Order of Operations
+a = 1
+b = 2
+c = 3 
+d = 4
+
+# Fully bracketed version of:  e = a - b ** c // d + a % c
+e = (a-((b ** c) // d)) + (a % c)
+
+
+# Question 4: Formating
+temperature = 32.6
+print ("The temperate today is: {:.3f} degree celsius".format(temperature))
+
+# Question 5
+userAge = int(input("Enter your age: "))s
+userAge = userAge + 10
+
+print (f"You will soon be {userAge} years old")
